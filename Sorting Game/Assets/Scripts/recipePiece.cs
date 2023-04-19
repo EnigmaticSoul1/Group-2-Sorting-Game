686f791263b671bd73f22fbb1f9e2cf02abf92e7
cloudspctr <patricelo314@gmail.com>
2023-04-19 10:09:31 +0800

    Added recipePiece file

---
+++ Sorting Game/Assets/Scripts/recipePiece.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recipePiece : MonoBehaviour
+{
+    // Start is called before the first frame update
+        private int x;
+    private int y;
+
+    private int X {
+        get { return X; }
+    }
+
+    private int Y{
+        get { return Y; }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
