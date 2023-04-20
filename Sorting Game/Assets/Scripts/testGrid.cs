181adc027727f0315846618769ebd82d039650d4
EnigmaticSoul1 <105492907+EnigmaticSoul1@users.noreply.github.com>
2023-04-20 10:51:27 +0800

    Update testGrid.cs
    Added a GetMatch() function to get matching pieces. Additionally, the swapPieces() function will now only work if the swap results in a match.

--- Sorting Game/Assets/Scripts/testGrid.cs
+++ Sorting Game/Assets/Scripts/testGrid.cs
@@ -154,11 +154,16 @@ public class testGrid : MonoBehaviour
             pieces [piece1.X, piece1.Y] = piece2;
             pieces [piece2.X, piece2.Y] = piece1;
 
-            int piece1X = piece1.X;
-            int piece1Y = piece1.Y;
+            if (GetMatch (piece1, piece2.X, piece2.Y) != null || GetMatch (piece2, piece1.X, piece1.Y) != null) {
+                int piece1X = piece1.X;
+                int piece1Y = piece1.Y;
 
-            piece1.MovableComponent.Move(piece2.X, piece2.Y, fillTime);
-            piece2.MovableComponent.Move(piece1X, piece1Y, fillTime);
+                piece1.MovableComponent.Move(piece2.X, piece2.Y, fillTime);
+                piece2.MovableComponent.Move(piece1X, piece1Y, fillTime);
+            } else {
+                pieces [piece1.X, piece1.Y] = piece1;
+                pieces [piece2.X, piece2.Y] = piece2;
+            }
         }
     }
 
@@ -181,5 +186,163 @@ public class testGrid : MonoBehaviour
         {
             swapPieces(pressedPiece, enteredPiece);
         }
+    }
+
+    //Checking if there are matching pieces
+    public List<recipePiece> GetMatch(recipePiece piece, int newX, int newY)
+    {
+        if (piece.isRecipe()) {
+            recipeData.recipeType recipe = piece.RecipeComponent.Type;
+            List<recipePiece> horizontalPieces = new List<recipePiece> ();
+            List<recipePiece> verticalPieces = new List<recipePiece> ();
+            List<recipePiece> matchingPieces = new List<recipePiece> ();
+
+            //Check horizontally for matching pieces
+            horizontalPieces.Add(piece);
+            for (int dir = 0; dir <= 1; dir++) {
+                for (int xOffset = 1; xOffset < xDim; xOffset++) {
+                    int x;
+
+                    if (dir == 0) {
+                        x = newX - xOffset;
+                    } else {
+                        x = newX + xOffset;
+                    }
+
+                    if (x < 0 || x >= xDim) {
+                        break;
+                    }
+
+                    if (pieces[x, newY].isRecipe() && pieces[x, newY].RecipeComponent.Type == recipe) {
+                        horizontalPieces.Add(pieces[x, newY]);
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            if (horizontalPieces.Count >=  3) {
+                for (int i = 0; i < horizontalPieces.Count; i++) {
+                    matchingPieces.Add(horizontalPieces[i]);
+                } 
+            }
+
+            if (matchingPieces.Count >= 3) {
+                return matchingPieces;
+            }
+
+            //Traverse vertically if we found a match (for L and T shape)
+            if (horizontalPieces.Count >= 3) {
+                for (int i = 0; i < horizontalPieces.Count; i++) {
+                    for (int dir = 0; dir <= 1; dir++) {
+                        for (int yOffset = 1; yOffset < yDim; yOffset++) {
+                            int y;
+
+                            if (dir == 0) { //Up
+                                y = newY - yOffset;
+                            } else { //Down
+                                y = newY + yOffset;
+                            }
+
+                            if (y < 0 || y >= yDim) {
+                                break;
+                            }
+
+                            if (pieces[horizontalPieces[i].X, y].isRecipe() && pieces[horizontalPieces[i].X, y].RecipeComponent.Type == recipe) {
+                                verticalPieces.Add(pieces[horizontalPieces[i].X, y]);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (verticalPieces.Count < 2) {
+                        verticalPieces.Clear();
+                    } else {
+                        for (int j = 0; j < verticalPieces.Count; j++) {
+                            matchingPieces.Add(verticalPieces[j]);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            //Check vertically for matching pieces
+            horizontalPieces.Clear();
+            verticalPieces.Clear();
+
+            verticalPieces.Add(piece);
+            for (int dir = 0; dir <= 1; dir++) {
+                for (int yOffset = 1; yOffset < yDim; yOffset++) {
+                    int y;
+
+                    if (dir == 0) {
+                        y = newY - yOffset;
+                    } else {
+                        y = newY + yOffset;
+                    }
+
+                    if (y < 0 || y >= yDim) {
+                        break;
+                    }
+
+                    if (pieces[y, newX].isRecipe() && pieces[y, newX].RecipeComponent.Type == recipe) {
+                        horizontalPieces.Add(pieces[y, newX]);
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            if (verticalPieces.Count >=  3) {
+                for (int i = 0; i < verticalPieces.Count; i++) {
+                    matchingPieces.Add(verticalPieces[i]);
+                } 
+            }
+
+            //Traverse horizontally if we found a match (for L and T shape)
+            if (verticalPieces.Count >= 3) {
+                for (int i = 0; i < verticalPieces.Count; i++) {
+                    for (int dir = 0; dir <= 1; dir++) {
+                        for (int xOffset = 1; xOffset < xDim; xOffset++) {
+                            int x;
+
+                            if (dir == 0) { //Up
+                                x = newX - xOffset;
+                            } else { //Down
+                                x = newX + xOffset;
+                            }
+
+                            if (x < 0 || x >= xDim) {
+                                break;
+                            }
+
+                            if (pieces[verticalPieces[i].Y, x].isRecipe() && pieces[verticalPieces[i].Y, x].RecipeComponent.Type == recipe) {
+                                verticalPieces.Add(pieces[verticalPieces[i].Y, x]);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (horizontalPieces.Count < 2) {
+                        horizontalPieces.Clear();
+                    } else {
+                        for (int j = 0; j < horizontalPieces.Count; j++) {
+                            matchingPieces.Add(horizontalPieces[j]);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            if (matchingPieces.Count >= 3) {
+                return matchingPieces;
+            }
+        }
+
+        return null;
     }
 }
